c5b5c2a5654877474b9a4c8fe7d7e1dc86f5ac82
vorovalex <vorovalex@gmail.com>
2019-05-02 18:56:23 +0300

    init

--- src/com/company/lesson_02/Test_06.java
+++ src/com/company/lesson_02/Test_06.java
@@ -19,5 +19,4 @@ public class Test_06 {
         if  (num>num1 && num>num2){ System.out.println("Max number: " + num); }
         else if(num1>num && num1>num2){ System.out.println("Max number: " + num1); }
         else  { System.out.println("Max number: " + num2);}
-    }
-}
+    }}
