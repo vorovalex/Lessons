f211a2b4d1af929d89f81889cf292d9f135fce7b
vorovalex <vorovalex@gmail.com>
2019-05-02 19:38:13 +0300

    init

--- src/com/company/lesson_02/Test_08.java
+++ src/com/company/lesson_02/Test_08.java
@@ -1,8 +1,20 @@
 package com.company.lesson_02;
 
-public class Test_08 {/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
 Ввести с клавиатуры отдельно Имя, число1, число2. Вывести надпись: «Имя» получает «число1» через «число2» лет.
 Пример:
 Коля получает 3000 через 5 лет.
 */
+public class Test_08 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int in1 = Integer.parseInt(bf.readLine());
+        int in2 = Integer.parseInt(bf.readLine());
+        System.out.println("Коля получает "+ in1 + " через " + in2 + " лет." );
+    }
 }
--- src/com/company/lesson_02/Test_09.java
+++ src/com/company/lesson_02/Test_09.java
@@ -1,6 +1,32 @@
 package com.company.lesson_02;
 
-public class Test_09 {/*
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
 Ввести с клавиатуры три числа, вывести на экран среднее из них.
 */
+public class Test_09 {
+    public static void main(String [] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = Integer.parseInt(bf.readLine());
+        int num2 = Integer.parseInt(bf.readLine());
+        int num3 = Integer.parseInt(bf.readLine());
+
+        if (num1>num2 && num2>num3){
+            System.out.println(num2); }
+        else if(num2>num1 && num1>num3){
+            System.out.println(num1); }
+        else if (num3>num1 && num3<num2){
+            System.out.println(num3);}
+        else if(num2>num1 && num2<num3){
+            System.out.println(num2); }
+        else if(num1>num2 && num1<num3){
+            System.out.println(num1);
+        }
+        else{
+            System.out.println(num3);}
+    }
 }
