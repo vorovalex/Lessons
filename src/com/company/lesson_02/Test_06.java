a231ab05703e44f4a63c731ce15ede56eb3f148d
vorovalex <vorovalex@gmail.com>
2019-05-02 18:55:45 +0300

    init

--- src/com/company/lesson_02/Test_06.java
+++ src/com/company/lesson_02/Test_06.java
@@ -3,26 +3,21 @@ package com.company.lesson_02;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
-public class Test_06 {
-    public static void main(String[] args) throws IOException {
-     /*
+/*
     Ввести с клавиатуры три числа, вывести на экран максимальное из них.
 */
+public class Test_06 {
+    public static void main(String[] args) throws IOException {
+
         BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("enter num:");
+        System.out.println("enter num (1):");
         int num = Integer.parseInt(bf.readLine());
-        System.out.println("enter num:");
+        System.out.println("enter num (2):");
         int num1 = Integer.parseInt(bf.readLine());
-        System.out.println("enter num:");
+        System.out.println("enter num (3):");
         int num2 = Integer.parseInt(bf.readLine());
-        if (num > num1){
-            System.out.println(num);
-        }
-        else if (num1>num2){
-            System.out.println(num1);
-        }
-           else {
-            System.out.println(num2); }
+        if  (num>num1 && num>num2){ System.out.println("Max number: " + num); }
+        else if(num1>num && num1>num2){ System.out.println("Max number: " + num1); }
+        else  { System.out.println("Max number: " + num2);}
     }
 }
---
+++ src/com/company/lesson_02/Test_09.java
@@ -0,0 +1,6 @@
+package com.company.lesson_02;
+
+public class Test_09 {/*
+Ввести с клавиатуры три числа, вывести на экран среднее из них.
+*/
+}
